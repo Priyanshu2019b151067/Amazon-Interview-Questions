9edb97aa2bf482bfc90b101bcd1c02c0671ba7d8
Priyanshu <priyanshukingh@gmail.com>
2022-06-09 00:18:44 +0530

    Partition Array

---
+++ partition_array_in_3_parts.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool canThreePartsEqualSum(vector<int>& A) {
+       int sum = 0;
+        for (int num : A) {
+            sum += num;
+        }
+        if (sum % 3 != 0) {
+            return false;
+        }
+        int part = 0;
+        int count = 0;
+        for (int num : A) {
+            part += num;
+            if (part == sum / 3) {
+                part = 0;
+                count++;
+                if (count == 3) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
